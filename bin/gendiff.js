3adfd04f8b74c9defbeb247282d4412ed413d5f2
AlmDea <inspired.by.summer@yandex.ru>
2022-06-09 15:07:42 -0400

    fifth step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,7 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('-f, --format <type>', 'output format', 'plain')
   .action((filepath1, filepath2, options) => {
     console.log(generateDiff(filepath1, filepath2, options.format));
   });
